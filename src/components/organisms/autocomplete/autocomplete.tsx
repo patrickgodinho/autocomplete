25f8e4402b58fd4d5b93e05fa6d43c14ae5923d2
Patrick Godinho <godinhopatrick@gmail.com>
2022-08-17 19:04:18 -0300

    loader and styles

--- src/components/organisms/autocomplete/autocomplete.tsx
+++ src/components/organisms/autocomplete/autocomplete.tsx
@@ -4,11 +4,13 @@ import { Suggestions } from '../../molecules/suggestions';
 import React, { useState } from 'react';
 import { CountriesService } from '../../../services/countries';
 import { useDebounce } from '../../../libs/debounce/debounce';
+import { Loader } from '../../atoms/loader';
 
 function Autocomplete() {
   const [suggestions, setSuggestions] = useState([]);
   const [value, setValue] = useState('');
   const [isSelected, setIsSelected] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
@@ -25,16 +27,19 @@ function Autocomplete() {
 
   const refreshSuggestions = async () => {
     if (isSelected) return;
+    setIsLoading(true);
     const countries = await CountriesService.getByName(value);
     setSuggestions(countries.map((country: any) => ({ text: country.name, onSelect: onSelect(country.name) })));
+    setIsLoading(false);
   };
 
   useDebounce(value, refreshSuggestions, 400);
 
   return (
     <div className="autocomplete-container">
-      <Input onChange={onChange} value={value} autoFocus placeholder="Search for countries..."/>
+      <Input onChange={onChange} value={value} autoFocus placeholder="Search for countries..." />
       <Suggestions suggestions={suggestions} highlight={value} />
+      {isLoading && <Loader />}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Home } from './components/pages/home';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement!);
